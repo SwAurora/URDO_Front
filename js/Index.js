9f01f375c4515348ba8ad2def848890fca378927
rlatj <rlatjddnr74@gmail.com>
2022-06-16 15:43:28 +0900

    2022-06-16 master(ver3)

--- js/Index.js
+++ js/Index.js
@@ -10,8 +10,8 @@ function moving_sidebar()
         nav.style.transform = 'translateX(-100%)';
         leftArrow.style.transform = 'scale(-1)';
         leftArrow.style.transition = '0.8s';
-        wrap.style.marginLeft='20px';
-        wrap.style.transition = '0.8s'
+        wrap.style.marginLeft = '20px';
+        wrap.style.transition = '0.8s';
         cnt++;
     }
     else
@@ -19,7 +19,7 @@ function moving_sidebar()
         nav.style.transform = 'translateX(0%)';
         leftArrow.style.transform = 'scale(1)';
         leftArrow.style.transition = '0.8s';
-        wrap.style.marginLeft='270px';
+        wrap.style.marginLeft = '270px';
         cnt--;
     }
 }
@@ -53,23 +53,49 @@ let chk1 = document.getElementById('check1');
 let chk2 = document.getElementById('check2');
 let chk3 = document.getElementById('check3');
 let chk4 = document.getElementById('check4');
-let span = document.getElementsByTagName('span');
+let chk5 = document.getElementById('check5');
+let span = document.querySelectorAll('.depth1 span');
+let menuIcon = document.querySelectorAll('.menuIcon');
+let bodyChk = document.querySelector('body');
+
+function getColor(color)
+{
+    let r = document.querySelector(":root");
+    let rs = getComputedStyle(r); //암기
+    return rs.getPropertyValue(color);
+}
+
+function getDarkColor(color)
+{
+    let r = document.querySelector("body.dark");
+    let rs = getComputedStyle(r); //암기
+    return rs.getPropertyValue(color);
+}
+
 chk1.addEventListener('change', function()
 {
     chk2.checked = false;
     chk3.checked = false;
     chk4.checked = false;
+    chk5.checked = false;
     radioReset(span);
     if(this.checked)
     {
-        span[8].innerHTML = '&#65293'; //full-width하이픈
-        span[9].style.color = '#6B6ECA';
-        span[9].style.fontWeight = 'bold';
+        span[0].innerHTML = '&#65293'; //full-width하이픈
+        span[1].style.color = getColor('--color-point');
+        span[1].style.fontWeight = 'bold';
     }
     else
     {
-        span[8].innerHTML = '&#65291';
-        span[9].style.color = '#616161';
+        span[0].innerHTML = '&#65291';
+        if(bodyChk.classList.contains('dark'))
+        {
+            span[1].style.color = getDarkColor('--color-text');
+        }
+        else
+        {
+            span[1].style.color = getColor('--color-text');
+        }
     }
 });
 chk2.addEventListener('change', function()
@@ -77,17 +103,25 @@ chk2.addEventListener('change', function()
     chk1.checked = false;
     chk3.checked = false;
     chk4.checked = false;
+    chk5.checked = false;
     radioReset(span);
     if(this.checked)
     {
-        span[10].innerHTML = '&#65293';
-        span[11].style.color = '#6B6ECA';
-        span[11].style.fontWeight = 'bold';
+        span[2].innerHTML = '&#65293';
+        span[3].style.color = getColor('--color-point');
+        span[3].style.fontWeight = 'bold';
     }
     else
     {
-        span[10].innerHTML = '&#65291';
-        span[11].style.color = '#616161';
+        span[2].innerHTML = '&#65291';
+        if(bodyChk.classList.contains('dark'))
+        {
+            span[3].style.color = getDarkColor('--color-text');
+        }
+        else
+        {
+            span[3].style.color = getColor('--color-text');
+        }
     }
 });
 chk3.addEventListener('change', function()
@@ -95,17 +129,25 @@ chk3.addEventListener('change', function()
     chk1.checked = false;
     chk2.checked = false;
     chk4.checked = false;
+    chk5.checked = false;
     radioReset(span);
     if(this.checked)
     {
-        span[12].innerHTML = '&#65293';
-        span[13].style.color = '#6B6ECA';
-        span[13].style.fontWeight = 'bold';
+        span[4].innerHTML = '&#65293';
+        span[5].style.color = getColor('--color-point');
+        span[5].style.fontWeight = 'bold';
     }
     else
     {
-        span[12].innerHTML = '&#65291';
-        span[13].style.color = '#616161';
+        span[4].innerHTML = '&#65291';
+        if(bodyChk.classList.contains('dark'))
+        {
+            span[5].style.color = getDarkColor('--color-text');
+        }
+        else
+        {
+            span[5].style.color = getColor('--color-text');
+        }
     }
 });
 chk4.addEventListener('change', function()
@@ -113,26 +155,68 @@ chk4.addEventListener('change', function()
     chk1.checked = false;
     chk2.checked = false;
     chk3.checked = false;
+    chk5.checked = false;
     radioReset(span);
     if(this.checked)
     {
-        span[14].innerHTML = '&#65293';
-        span[15].style.color = '#6B6ECA';
-        span[15].style.fontWeight = 'bold';
+        span[6].innerHTML = '&#65293';
+        span[7].style.color = getColor('--color-point');
+        span[7].style.fontWeight = 'bold';
     }
     else
     {
-        span[14].innerHTML = '&#65291';
-        span[16].style.color = '#616161';
+        span[6].innerHTML = '&#65291';
+        if(bodyChk.classList.contains('dark'))
+        {
+            span[7].style.color = getDarkColor('--color-text');
+        }
+        else
+        {
+            span[7].style.color = getColor('--color-text');
+        }
+    }
+});
+
+chk5.addEventListener('change', function()
+{
+    chk1.checked = false;
+    chk2.checked = false;
+    chk3.checked = false;
+    chk4.checked = false;
+    radioReset(span);
+    if(this.checked)
+    {
+        span[8].innerHTML = '&#65293';
+        span[9].style.color = getColor('--color-point');
+        span[9].style.fontWeight = 'bold';
+    }
+    else
+    {
+        span[8].innerHTML = '&#65291';
+        if(bodyChk.classList.contains('dark'))
+        {
+            span[9].style.color = getDarkColor('--color-text');
+        }
+        else
+        {
+            span[9].style.color = getColor('--color-text');
+        }
     }
 });
 
 function radioReset(span)
 {
-    for(let i = 6; i <= 14; i += 2)
+    for(let i = 0; i <= 8; i += 2)
     {
         span[i].innerHTML = '&#65291';
-        span[i + 1].style.color = '#616161';
+        if(bodyChk.classList.contains('dark'))
+        {
+            span[i + 1].style.color = getDarkColor('--color-text');
+        }
+        else
+        {
+            span[i + 1].style.color = getColor('--color-text');
+        }
         span[i + 1].style.fontWeight = 'normal';
     }
 }
@@ -144,4 +228,64 @@ const modeSwitch = body.querySelector(".toggle-switch");
 modeSwitch.addEventListener("click", () =>
 {
     body.classList.toggle("dark");
+    let body1 = document.querySelector('body');
+    let chk = body1.classList.contains('dark');
+    localStorage.setItem('whatMode', String(chk));
+    for(let i = 0; i <= 8; i += 2)
+    {
+        if(span[i].innerHTML !== '－')
+        {
+            if(bodyChk.classList.contains('dark'))
+            {
+                span[i + 1].style.color = getDarkColor('--color-text');
+            }
+            else
+            {
+                span[i + 1].style.color = getColor('--color-text');
+            }
+        }
+    }
+
+    if(!chk)
+    {
+        for(const MI of menuIcon)
+        {
+            MI.style.filter = 'none';
+        }
+    }
+    else
+    {
+        for(const MI of menuIcon)
+        {
+            MI.style.filter = 'brightness(0) saturate(100%) invert(88%) sepia(4%) saturate(5%) hue-rotate(192deg) brightness(102%) contrast(90%)';
+        }
+    }
 });
+
+window.addEventListener('beforeunload', function()
+{
+    chk1.checked = false;
+    chk2.checked = false;
+    chk3.checked = false;
+    chk4.checked = false;
+    chk5.checked = false;
+})
+
+document.addEventListener('DOMContentLoaded', function()
+{
+    const body = document.querySelector('body');
+    const whatMode = localStorage.getItem('whatMode'); //whatMode 아이템 값 불러오기
+
+    if(whatMode === "false")
+    { // 라이트 모드 라면
+        return !1;
+    }
+    else
+    { // 다크 모드 라면
+        body.classList.toggle("dark");
+        for(const MI of menuIcon)
+        {
+            MI.style.filter = 'brightness(0) saturate(100%) invert(88%) sepia(4%) saturate(5%) hue-rotate(192deg) brightness(102%) contrast(90%)';
+        }
+    }
+})
