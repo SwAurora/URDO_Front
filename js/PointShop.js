35d61e84dd05b9f380ae9b2588551463f496ed58
Kanghyeonseon <102352988+Kanghyeonseon@users.noreply.github.com>
2022-06-17 17:15:24 +0900

    hyeonseon

--- js/PointShop.js
+++ js/PointShop.js
@@ -1,12 +1,20 @@
-$(document).ready(function() {
-  $('.tabBtn').on('click',function() {
+$(document).ready(function () {
+  $('.tabBtn').on('click', function () {
     idx = $(this).index();
     $('.tabBtn').removeClass('on');
     $('.tabBtn').eq(idx).addClass('on');
     $('.tabContents').removeClass('on');
     $('.tabContents').eq(idx).addClass('on');
   })
-  
 
 
-}); 
+  $('.page_nation').on('click', function() {
+    alert("실행!");
+  })
+
+});
+
+
+
+
+
