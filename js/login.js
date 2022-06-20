ad1e50b139b4c044c50ff0ff66e7762db1a9b5d9
rlatj <rlatjddnr74@gmail.com>
2022-06-20 17:07:15 +0900

    2022-06-20 master(ver8)

--- js/login.js
+++ js/login.js
@@ -6,13 +6,13 @@ function showHide()
     if(password.type === 'password')
     {
         password.setAttribute('type', 'text');
-        toggle.style.background = 'url(../img/loginEyeHidden.svg)';
+        toggle.style.background = 'url(../img/login/loginEyeHidden.svg)';
         toggle.style.backgroundSize = 'cover';
     }
     else
     {
         password.setAttribute('type', 'password');
-        toggle.style.background = 'url(../img/loginEye.svg)';
+        toggle.style.background = 'url(../img/login/loginEye.svg)';
         toggle.style.backgroundSize = 'cover';
         toggle.classList.remove('hide');
     }
