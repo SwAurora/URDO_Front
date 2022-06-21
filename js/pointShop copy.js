7263ca2ee635db13ff33d42aafa01940ded3477f
Kanghyeonseon <102352988+Kanghyeonseon@users.noreply.github.com>
2022-06-22 01:40:04 +0900

    hyeonseon

--- js/pointShop copy.js
+++ js/pointShop copy.js
@@ -1,9 +1,7 @@
 $(document).ready(function () {
-
-
   $('.menu-level2 li').on('click', function() {
+    // menu-level2 : 실시간인기, 일간인기, 주간인기
     idx = $(this).index();
-    alert(idx);
     $('.l2-list').removeClass('on');
     $('.l2-list').eq(idx).addClass('on');
     $('.tabContents').removeClass('on');
@@ -11,12 +9,31 @@ $(document).ready(function () {
   })
 
 
+  //탭 인덱스를 받아서 그에 해당하는 페이지를 출력하는 함수
+    $('.page_num').on('click', function() {
+      let idx2 = $(this).index();
+      $('.page_num').removeClass('on');
+      $('.page_num').eq(idx2).addClass('on');
+      if(idx==0) {
+        $('.tab1_page').removeClass('on');
+        $('.tab1_page').eq(idx2).addClass('on');
+      } else if(idx==1) {
+        $('.tab2_page').removeClass('on');
+        $('.tab2_page').eq(idx2).addClass('on');
+      } else if(idx==2) {
+        $('.tab3_page').removeClass('on');
+        $('.tab3_page').eq(idx2).addClass('on');
+      }
+  })
+
+
+
+
   $('#prev').on('click', function() {
     idx3=$('.page_num.on').index();
     if(idx3-1 >=0) {
       $('.page_num').removeClass('on');
       $('.page_num').eq(idx3-1).addClass('on');
-      // 페이지컨텐츠를 구성하지 않았음
     }
   })
   $('#next').on('click', function() {
@@ -25,10 +42,8 @@ $(document).ready(function () {
     if(idx3+1 <= end) {
       $('.page_num').removeClass('on');
       $('.page_num').eq(idx3+1).addClass('on');
-      // 페이지컨텐츠를 구성하지 않았음
     }
   })
-
 });
 
 
